1ed902ae064ac768faea480218443c7324c58cfc
Alazo4872 <lazoal4872@gmail.com>
2025-09-28 20:29:02 -0500

    Included delete and update APIs Removed local test database Added error handlers for malformatted id

--- index.js
+++ index.js
@@ -8,30 +8,14 @@ app.use(express.static('dist'))
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if(error.name === 'CastError'){
+        return response.status(400).send({error: 'Malformated id'})
     }
-]
+    next(error)
+}
+
 
 app.get('/api/persons', (request, response) => {
     Phonebook.find({}).then(persons =>{
@@ -40,11 +24,13 @@ app.get('/api/persons', (request, response) => {
 })
 
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     const time = Date();
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>  
+    Phonebook.countDocuments({}).then(count => {
+        response.send(`<p>Phonebook has info for ${count} people</p>  
                     <p>${time}</p>`)
-
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -52,9 +38,6 @@ app.post('/api/persons', (request, response) => {
 
     if((!body.name) || !body.number){
         return response.status(400).json({error: 'content missing'})
-    }
-    else if(persons.find(p => p.name === body.name)){
-        return response.status(400).json({error: 'name already exists'})
     }
     
     const person = new Phonebook ({
@@ -67,24 +50,42 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Phonebook.findById(request.params.id).then(person => {
+        if(!person){
+            return response.status(404).end()
+        }
         response.json(person)
-    })
+    }).catch(error => next(error))
 })
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
-    
-    response.status(204).end()
-    
+app.delete('/api/persons/:id', (request, response, next) => {
+    Phonebook.findByIdAndDelete(request.params.id).then(result => {
+            response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const {name, number} = request.body
+    Phonebook.findById(request.params.id).then(person => {
+        if(!person){
+            return response.status(404).end()
+        }
+        person.name = name
+        person.number = number
+
+        return person.save().then(updatedNote => {
+            response.json(updatedNote)
+        })
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () =>
     console.log(`server running on port ${PORT}`)
 )
