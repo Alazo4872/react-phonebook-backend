ba49ef1b49ef04e7f39dcf4de8f70db45b00c5cb
Alazo4872 <lazoal4872@gmail.com>
2025-09-30 00:26:20 -0500

    custom validator for phone formatting

--- models/phonebook.js
+++ models/phonebook.js
@@ -12,7 +12,15 @@ const phonebookSchema = new mongoose.Schema({
       minLength: 3,
       required: true
     },
-    number: String
+    number: {type: String,
+      minLength:8,
+      required: true,
+      validate: {validator: function(v){
+        return /^\d{2,3}-\d+$/.test(v)
+      }, 
+      message: props => `${props.value} is not a valid phone number! Format is 2-3 digits, dash, then remaining digits`
+    }
+    }
 })
 
 phonebookSchema.set('toJSON', {
